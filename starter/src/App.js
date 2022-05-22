f2a39690a005df37f5ec6e3ff34a00da07d59ff1
Jee Ryann Genio <jeegenio@gmail.com>
2022-05-23 02:53:29 +0800

    FEATURES: added components and css classes

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
